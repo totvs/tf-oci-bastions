04b35fc56ee813a208955907b9c7911418d97466
Thiago Marcel Barreto G Accessor <87589065+thiagoaccessor@users.noreply.github.com>
2026-04-01 16:22:37 -0300

    Ajuste nas variáveis, e atualização da versão v1.1

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 resource "oci_bastion_bastion" "this" {
-  compartment_id                = each.value.bastion_compartment_id
-  target_subnet_id              = each.value.bastion_target_subnet_id
-  client_cidr_block_allow_list  = each.value.bastion_client_cidr_block_allow_list
-  freeform_tags                 = each.value.bastion_freeform_tags
-  # display_name                = each.value.display_name # se quiser customizar
+  for_each = var.oci_bastions
+
+  name                         = each.key
+  bastion_type                 = each.value.bastion_type
+  compartment_id               = each.value.bastion_compartment_id
+  target_subnet_id             = each.value.bastion_target_subnet_id
+  client_cidr_block_allow_list = each.value.bastion_client_cidr_block_allow_list
+  freeform_tags                = try(each.value.bastion_freeform_tags, null)
+  defined_tags                 = try(each.value.bastion_defined_tags, null)
 }
-
-
