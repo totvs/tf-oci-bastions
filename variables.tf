35138ff1a47fef40d0074526dd48cec2e4e66e53
Thiago Marcel Barreto G Accessor <87589065+thiagoaccessor@users.noreply.github.com>
2026-04-01 16:25:53 -0300

    Ajuste na variable

--- variables.tf
+++ variables.tf
@@ -1,25 +1,5 @@
-variable "compartment_id" {
-  description = "OCID do compartimento onde o Bastion será criado."
-  type        = string
-}
-
-variable "target_subnet_id" {
-  description = "OCID da subnet alvo onde o Bastion terá acesso."
-  type        = string
-}
-
-variable "client_cidr_block_allow_list" {
-  description = <<EOT
-Lista de blocos CIDR permitidos para acessar o Bastion.
-Por padrão, permite acesso de qualquer IP (0.0.0.0/0).
-ATENÇÃO: Por segurança, sobrescreva este valor para restringir o acesso conforme sua política.
-EOT
-  type    = list(string)
-  default = ["0.0.0.0/0"]
-}
-
-variable "freeform_tags" {
-  description = "Mapa de tags livres para padronização e identificação de recursos."
-  type        = map(string)
+variable "oci_bastions" {
+  description = "Mapa de objetos contendo as configurações de todos os Bastions a serem criados."
+  type        = any
   default     = {}
 }
